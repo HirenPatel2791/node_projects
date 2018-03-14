9a4347c2008e067b7786bf4e52b36eb7f671dfc4
HirenPatel2791 <hiren.patel@scanbuy.com>
2018-03-14 15:19:23 -0400

    middleware and logging

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 var app = express();
 
 app.set('view engine', 'hbs');
 
 hbs.registerPartials(__dirname + '/views/partials/');
 //middle ware piece
+
+
+//express middleware
+app.use((req,res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log',log + '\n', (err)=> {
+    if (err) {
+      console.log('Unable to append server.log');
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
 //serving static directory
 app.use(express.static(__dirname + '/public'));
 
